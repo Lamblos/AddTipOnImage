ee0e76338e8935e1a995d3048ef2b56d43b33bb9
arvin <rorogestudio@gmail.com>
2014-12-31 10:30:24 +0800

    add class

---
+++ AddTipOnImage/AddTipOnImageClass/RGImageView.h
@@ -0,0 +1,13 @@
+//
+//  RGImageView.h
+//  AddTipOnImage
+//
+//  Created by Arvin on 14/12/31.
+//  Copyright (c) 2014年 com.roroge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGImageView : UIImageView
+
+@end
---
+++ AddTipOnImage/AddTipOnImageClass/RGImageView.m
@@ -0,0 +1,21 @@
+//
+//  RGImageView.m
+//  AddTipOnImage
+//
+//  Created by Arvin on 14/12/31.
+//  Copyright (c) 2014年 com.roroge. All rights reserved.
+//
+
+#import "RGImageView.h"
+
+@implementation RGImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
